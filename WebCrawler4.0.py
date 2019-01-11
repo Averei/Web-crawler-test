f0e2aeb763863e40af070971bfe4ae91a0d89d0e
Averei <raveen.edward087@gmail.com>
2019-01-11 12:28:41 +0800

    Update WebCrawler4.0.py

--- WebCrawler4.0.py
+++ WebCrawler4.0.py
@@ -1,120 +1,64 @@
-from requests import get
-from bs4 import BeautifulSoup
-from pprint import pprint
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+from itertools import islice
 import pandas as pd
 from time import sleep
 
-
-url = 'https://forum.lowyat.net/ReviewsandGuides'
-
-list_topic = []
-list_description = []
-list_replies = []
-list_topicStarted = []
-list_totalViews = []
+SESSION = requests.Session()
 
 
-def getContentFromURL(_url):
+def get_table_rows(base_url, posts_per_page=30):
+    """Continously yield rows from the posts table.
+
+    Requests a new page only when needed.
+    """
+    start_at = 0
+    while True:
+        print(f'current page index is: {start_at // posts_per_page + 1}')
+        response = SESSION.get(base_url + f"/+{start_at}")
+        print(response)
+        soup = BeautifulSoup(response.text, 'lxml',
+                             parse_only=SoupStrainer("table", {"cellspacing": "1"}))
+        yield from soup.find_all("tr")
+        start_at += posts_per_page
+
+
+def parse_row(row):
+    """Get info from a row"""
+    columns = row.select("td")
     try:
-        response = get(_url)
-        html_soup = BeautifulSoup(response.text, 'lxml')
-        return html_soup
-    except Exception as e:
-        print('Error.getContentFromURL:', e)
-        return None
-
-
-def iterateThroughPages(_lastindexpost, _postperpage, _url):
-    indices = '/+'
-    index = 0
-    for i in range(index, _lastindexpost):
-        print('Getting data from ' + url)
-        try:
-            extractDataFromRow1(getContentFromURL(
-                _url), 'td', 'row1', 'valign', 'middle')
-            extractDataFromRow2(getContentFromURL(_url))
-            print('current page index is: ' + str(index))
-            print(_url)
-            while i <= _lastindexpost:
-                for table in get(_url):
-                    if table != None:
-                        new_getPostPerPage = i + _postperpage
-                        newlink = f'{url}{indices}{new_getPostPerPage}'
-                        print(newlink)
-                        bs_link = getContentFromURL(newlink)
-                        extractDataFromRow1(
-                            bs_link, 'td', 'row1', 'valign', 'middle')
-                        extractDataFromRow2(bs_link)
-                        # threading to prevent crash. Waits 0.5 secs before executing
-                        sleep(0.5)
-                    i += _postperpage
-                    print('current page index is: ' + str(i))
-                    if i > _lastindexpost:
-                        # If i gets more than the input page(etc 1770) halts
-                        print('No more available post to retrieve')
-                        return
-        except Exception as e:
-            print('Error.iterateThroughPages:', e)
-            return None
-
-
-def extractDataFromRow1(_url, _tag, _classname, _alignment, _position):
+        if not columns or columns[0]["class"] in (["darkrow1"], ["nopad"]):
+            return
+    except KeyError:  # first column has no class
+        return
     try:
-        for container in _url.find_all(_tag, {'class': _classname, _alignment: _position}):
-            # get data from topic title in table cell
-            topic = container.select_one(
-                'a[href^="/topic/"]').text.replace("\n", "")
-            description = container.select_one(
-                'div.desc').text.replace("\n", "")
-            if topic or description is not None:
-                dict_topic = topic
-                dict_description = description
-                if dict_description is '':
-                    dict_description = 'No Data'
-                    # list_description.append(dict_description)
-                    #so no empty string#
-                list_topic.append(dict_topic)
-                list_description.append(dict_description)
-            else:
-                None
-    except Exception as e:
-        print('Error.extractDataFromRow1:', e)
-        return None
+        title = row.select_one(
+            "td.row1 a[href^=/topic/]").text.strip() or "No Data"
+        description = row.select_one(
+            "td.row1 div.desc").text.strip() or "No Data"
+        replies = row.select_one("td:nth-of-type(4)").text.strip() or "No Data"
+        topic_starter = row.select_one(
+            'td:nth-of-type(5)').text.strip() or "No Data"
+        total_views = row.select_one(
+            'td:nth-of-type(6)').text.strip() or "No Data"
+        print(title)
+        sleep(0.3)
+    except AttributeError:  # something is None
+        return
+    return {"Title": title,
+            "Description": description,
+            "Replies": replies,
+            "Topic Starter": topic_starter,
+            "Total Views": total_views}
 
 
-def extractDataFromRow2(_url):
-    try:
-        for container in _url.select('table[cellspacing="1"] > tr')[2:32]:
-            replies = container.select_one('td:nth-of-type(4)').text.strip()
-            topic_started = container.select_one(
-                'td:nth-of-type(5)').text.strip()
-            total_views = container.select_one(
-                'td:nth-of-type(6)').text.strip()
-            if replies or topic_started or total_views is not None:
-                dict_replies = replies
-                dict_topicStarted = topic_started
-                dict_totalViews = total_views
-                if dict_replies is '':
-                    dict_replies = 'No Data'
-                elif dict_topicStarted is '':
-                    dict_topicStarted = 'No Data'
-                elif dict_totalViews is '':
-                    dict_totalViews = 'No Data'
-                list_replies.append(dict_replies)
-                list_topicStarted.append(dict_topicStarted)
-                list_totalViews.append(dict_totalViews)
-            else:
-                print('no data')
-                None
-    except Exception as e:
-        print('Error.extractDataFromRow2:', e)
-        return None
+def parse_rows(url):
+    """Filter out rows that could not be parsed"""
+    yield from filter(None, (parse_row(row) for row in get_table_rows(url)))
 
 
-# limit to 1740
-print(iterateThroughPages(1740, 30, url))
-new_panda = pd.DataFrame(
-    {'Title': list_topic, 'Description': list_description,
-     'Replies': list_replies, 'Topic Starter': list_topicStarted, 'Total Views': list_totalViews})
-pprint(new_panda)
-new_panda.to_csv("output.csv")
+if __name__ == "__main__":
+    url = 'https://forum.lowyat.net/ReviewsandGuides'
+    max_posts = 1770
+    df = pd.DataFrame.from_records(islice(parse_rows(url), max_posts))
+    print(df)
